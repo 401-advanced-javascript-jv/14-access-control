3575e15118ec9fbcb89438db969989e0f4bed74d
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-01 14:53:47 -0700

    virutal population complete

--- src/auth/router.js
+++ src/auth/router.js
@@ -4,13 +4,15 @@ const express = require('express');
 const authRouter = express.Router();
 
 const User = require('./users-model.js');
+const Role = require('./roles-model.js');
 const auth = require('./middleware.js');
 const oauth = require('./oauth/google.js');
 
 authRouter.post('/signup', (req, res, next) => {
   let user = new User(req.body);
-  user.save()
-    .then( (user) => {
+  user
+    .save()
+    .then((user) => {
       req.token = user.generateToken();
       req.user = user;
       res.set('token', req.token);
@@ -25,17 +27,36 @@ authRouter.post('/signin', auth(), (req, res, next) => {
   res.send(req.token);
 });
 
-authRouter.get('/oauth', (req,res,next) => {
-  oauth.authorize(req)
-    .then( token => {
+authRouter.get('/oauth', (req, res, next) => {
+  oauth
+    .authorize(req)
+    .then((token) => {
       res.status(200).send(token);
     })
     .catch(next);
 });
 
-authRouter.post('/key', auth, (req,res,next) => {
+authRouter.post('/key', auth, (req, res, next) => {
   let key = req.user.generateKey();
   res.status(200).send(key);
+});
+
+authRouter.get('/populate-roles', (request, response, next) => {
+  const capabilities = {
+    admin: ['create', 'read', 'update', 'delete'],
+    editor: ['create', 'read', 'update'],
+    user: ['read'],
+  };
+
+  for (let roleType of Object.keys(capabilities)) {
+    let newRole = new Role({
+      role: roleType,
+      capabilities: capabilities[roleType],
+    });
+
+    newRole.save().catch( console.error );
+  }
+  response.status(200).send('roles created');
 });
 
 module.exports = authRouter;
