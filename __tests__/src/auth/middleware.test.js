407b20db611899c7ce73e0d6d3bb629aa36ea0ee
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-01 23:41:44 -0700

    fix tests, add a couple more

--- __tests__/src/auth/middleware.test.js
+++ __tests__/src/auth/middleware.test.js
@@ -1,23 +1,54 @@
 'use strict';
 
-process.env.SECRET='test';
+process.env.SECRET = 'test';
 
-const {startDB,stopDB} = require('../../supergoose.js');
+const { startDB, stopDB } = require('../../supergoose.js');
 const auth = require('../../../src/auth/middleware.js');
 const Users = require('../../../src/auth/users-model.js');
 const Roles = require('../../../src/auth/roles-model.js');
 
+// admin:password: YWRtaW46cGFzc3dvcmQ=
+// admin:foo: YWRtaW46Zm9v
+// editor:password: ZWRpdG9yOnBhc3N3b3Jk
+// user:password: dXNlcjpwYXNzd29yZA==
+// super:user : c3VwZXI6dXNlcg==
 let users = {
-  admin: {username: 'admin', password: 'password', role: 'admin'},
-  editor: {username: 'editor', password: 'password', role: 'editor'},
-  user: {username: 'user', password: 'password', role: 'user'},
+  admin: { username: 'admin', password: 'password', role: 'admin' },
+  editor: { username: 'editor', password: 'password', role: 'editor' },
+  user: { username: 'user', password: 'password', role: 'user' },
+  superuser: { username: 'super', password: 'user', role: 'superuser' },
+};
+
+let roles = {
+  user: { role: 'user', capabilities: ['read'] },
+  editor: { role: 'editor', capabilities: ['create', 'update', 'read'] },
+  admin: {
+    role: 'admin',
+    capabilities: ['create', 'update', 'read', 'delete'],
+  },
+  superuser: {
+    role: 'superuser',
+    capabilities: ['create', 'update', 'read', 'delete', 'superuser'],
+  },
+};
+
+let encodedBasic = {
+  admin: 'YWRtaW46cGFzc3dvcmQ=',
+  editor: 'ZWRpdG9yOnBhc3N3b3Jk',
+  user: 'dXNlcjpwYXNzd29yZA==',
+  super: 'c3VwZXI6dXNlcg==',
 };
 
 beforeAll(async (done) => {
   await startDB();
-  const admin = await new Users(users.admin).save();
-  const editor = await new Users(users.editor).save();
-  const user = await new Users(users.user).save();
+
+  for (let key of Object.keys(users)) {
+    await new Users(users[key]).save();
+  }
+  for (let key of Object.keys(roles)) {
+    await new Roles(roles[key]).save();
+  }
+
   done();
 });
 
@@ -29,20 +60,12 @@ afterAll(stopDB);
   ... you can go further as you please.
  */
 describe('Auth Middleware', () => {
-
-  // admin:password: YWRtaW46cGFzc3dvcmQ=
-  // admin:foo: YWRtaW46Zm9v
-  // editor:password: ZWRpdG9yOnBhc3N3b3Jk
-  // user:password: dXNlcjpwYXNzd29yZA==
-
   let errorMessage = 'Invalid User ID/Password';
 
   describe('user authentication', () => {
-
     let cachedToken;
 
     it('fails a login for a user (admin) with the incorrect basic credentials', () => {
-
       let req = {
         headers: {
           authorization: 'Basic YWRtaW46Zm9v',
@@ -52,15 +75,12 @@ describe('Auth Middleware', () => {
       let next = jest.fn();
       let middleware = auth();
 
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).toHaveBeenCalledWith(errorMessage);
-        });
-
+      return middleware(req, res, next).then(() => {
+        expect(next).toHaveBeenCalledWith(errorMessage);
+      });
     }); // it()
 
     it('fails a login for a user (admin) with an incorrect bearer token', () => {
-
       let req = {
         headers: {
           authorization: 'Bearer foo',
@@ -76,11 +96,9 @@ describe('Auth Middleware', () => {
       // behavior instead of a standard promise signature
       middleware(req, res, next);
       expect(next).toHaveBeenCalledWith(errorMessage);
-
     }); // it()
 
     it('logs in an admin user with the right credentials', () => {
-
       let req = {
         headers: {
           authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
@@ -90,19 +108,16 @@ describe('Auth Middleware', () => {
       let next = jest.fn();
       let middleware = auth();
 
-      return middleware(req,res,next)
-        .then( () => {
-          cachedToken = req.token;
-          expect(next).toHaveBeenCalledWith();
-        });
-
+      return middleware(req, res, next).then(() => {
+        cachedToken = req.token;
+        expect(next).toHaveBeenCalledWith();
+      });
     }); // it()
 
     // this test borrows the token gotten from the previous it() ... not great practice
     // but we're using an in-memory db instance, so we need a way to get the user ID
     // and token from a "good" login, and the previous passing test does provide that ...
     it('logs in an admin user with a correct bearer token', () => {
-
       let req = {
         headers: {
           authorization: `Bearer ${cachedToken}`,
@@ -112,26 +127,37 @@ describe('Auth Middleware', () => {
       let next = jest.fn();
       let middleware = auth();
 
-      return middleware(req,res,next)
-        .then( () => {
-          expect(next).toHaveBeenCalledWith();
-        });
-
+      return middleware(req, res, next).then(() => {
+        expect(next).toHaveBeenCalledWith();
+      });
     }); // it()
-
   });
 
   describe('user authorization', () => {
+    let cachedTokens = {};
+
+      let userSigninMiddleware = auth();
+      for ( let key of Object.keys( encodedBasic ) ) {
+        let request = { headers: { authorization: `Basic ${encodedBasic[key]}` } };
+        let response = {};
+        let next = jest.fn();
+        Promise.resolve(userSigninMiddleware( request, response, next ))
+          .then(() => cachedTokens[key] = request.token);
+      }
 
     it('restricts access to a valid user without permissions', () => {
+      let request = {
+        headers: { authorization: `Basic dXNlcjpwYXNzd29yZA==` },
+      };
+      let response = {};
+      let next = jest.fn();
+      let middleware = auth('delete');
 
+      return middleware(request, response, next).then(() => {
+        expect(next).toHaveBeenCalledWith(errorMessage);
+      });
     }); // it()
 
-    it('grants access when a user has permission', () => {
-
-    }); // it()
-
+    it('grants access when a user has permission', () => {}); // it()
   }); // describe()
-
 });
-
---
+++ __tests__/src/routes/routes.test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('empty test', () => {
+  it('passes', () => {});
+});
